335e47905f6939985a7ee0d3d3b405d5d78570fe
Allan Wazacz <cazwazacz@gmail.com>
2017-11-22 13:04:25 +0000

    Fulfills basic specs for thermostat

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,97 @@
+describe("Thermostat", function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe("#new", function() {
+    it("starts at 20 degrees", function() {
+      expect(thermostat._temp).toEqual(20);
+    });
+    it("has power saving mode on by default", function() {
+      expect(thermostat._isPowerSavingMode).toEqual(true);
+    });
+  })
+
+  describe("#temp", function() {
+    it("returns the temperature", function() {
+      expect(thermostat.temp()).toEqual(20);
+    });
+  });
+
+  describe("changing the temperature", function() {
+    it("increments temperature by one", function() {
+      thermostat.up();
+      expect(thermostat.temp()).toEqual(21);
+    });
+
+    it("does not let you decrement below 10", function() {
+      thermostat._temp = 10;
+      thermostat.down();
+      expect(thermostat._temp).toEqual(10);
+    });
+
+    it("decrements temperature by one", function() {
+      thermostat.down();
+      expect(thermostat.temp()).toEqual(19);
+    });
+
+    it("does not let you increment above max", function() {
+      thermostat._temp = 25;
+      spyOn(thermostat, 'max').and.returnValue(25);
+      thermostat.up();
+      expect(thermostat._temp).toEqual(25);
+    });
+  });
+
+  describe("#min", function() {
+    it("has a minimum temp of 10 degrees", function() {
+      expect(thermostat.min()).toEqual(10);
+    });
+  });
+
+  describe("#max", function() {
+    it("returns 25 when power saving mode is on", function() {
+      expect(thermostat.max()).toEqual(25);
+    });
+
+    it("returns 32 when power saving mode is off", function() {
+      thermostat._isPowerSavingMode = false;
+      expect(thermostat.max()).toEqual(32);
+    });
+  });
+
+  describe("#isPowerSavingMode", function() {
+    it("returns _isPowerSavingMode", function() {
+      expect(thermostat.isPowerSavingMode()).toEqual(true);
+    });
+  });
+
+  describe("#reset", function() {
+    it("resets temperature to 20", function() {
+      thermostat._temp = 25;
+      thermostat.reset();
+      expect(thermostat._temp).toEqual(20);
+    });
+  });
+
+  describe("#usage", function() {
+    it("returns low-usage if temp is below 18", function() {
+      thermostat._temp = 16;
+      expect(thermostat.usage()).toEqual("low-usage");
+    });
+
+    it("returns medium-usage if temp is below 18", function() {
+      thermostat._temp = 23;
+      expect(thermostat.usage()).toEqual("medium-usage");
+    });
+
+    it("returns high-usage if temp is below 18", function() {
+      thermostat._temp = 30;
+      expect(thermostat.usage()).toEqual("high-usage");
+    });
+  });
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,54 @@
+var SETTINGS = {
+  initialTemp: 20,
+  step: 1,
+  min: 10,
+  PSMax: 25,
+  max: 32,
+  lowUsage: 18,
+  mediumUsage: 25
+}
+
+function Thermostat (settings = SETTINGS) {
+  this.settings = settings;
+  this._temp = this.settings.initialTemp;
+  this._isPowerSavingMode = true;
+}
+
+
+Thermostat.prototype.temp = function () {
+  return this._temp;
+};
+
+Thermostat.prototype.up = function () {
+  this._temp = Math.min(this._temp + this.settings.step, this.max());
+};
+
+Thermostat.prototype.down = function () {
+  this._temp = Math.max(this._temp - this.settings.step, this.min());
+};
+
+Thermostat.prototype.min = function () {
+  return this.settings.min;
+};
+
+Thermostat.prototype.isPowerSavingMode = function () {
+  return this._isPowerSavingMode;
+};
+
+Thermostat.prototype.max = function () {
+  return this.isPowerSavingMode() ? this.settings.PSMax : this.settings.max;
+};
+
+Thermostat.prototype.reset = function () {
+  this._temp = this.settings.initialTemp;
+};
+
+Thermostat.prototype.usage = function () {
+  if (this._temp < this.settings.lowUsage) {
+    return "low-usage";
+  } else if (this._temp < this.settings.mediumUsage) {
+    return "medium-usage";
+  } else {
+    return "high-usage";
+  }
+};
