11b12cd357c284886b37b6e8545be1603726f428
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-22 16:58:08 +0000

    adds local weather

--- public/index.js
+++ public/index.js
@@ -1,6 +1,16 @@
 
 $(document).ready(function() {
   var thermostat = new Thermostat();
+  var url = 'http://www.openweathermap.org/data/2.5/weather';
+  var key = 'b1b15e88fa797225412429c1c50c122a1';
+
+  function assemble_url(city, key) {
+    return url + '?city=' + city + '&appid=' + key;
+  };
+  
+  $.getJSON(assemble_url('London,uk', key), function(data) {
+    $("#localTemp").html(data.main.temp);
+  });
 
   function update_ps_color() {
     var color = thermostat.isPowerSavingMode() ? "green" : "red";
