1ff99550b32b3efcd6de19a3a196d6407018cd6e
Allan Wazacz <cazwazacz@gmail.com>
2017-11-23 18:20:23 +0000

    Adds backend for api

--- public/index.js
+++ public/index.js
@@ -1,5 +1,11 @@
 
 $(document).ready(function() {
+
+  function postingTemp (temp) {
+    $.post('/temperature', {temperature: temp}, function(data) {
+    });
+  }
+
   var thermostat = new Thermostat();
   var url = 'http://www.openweathermap.org/data/2.5/weather';
   var key = 'b1b15e88fa797225412429c1c50c122a1';
@@ -12,14 +18,12 @@ $(document).ready(function() {
     'error': function() { alert('Chosen city does not exist') }
   });
 
-  function getLocalTemp (city = 'London,uk') {
+  function getLocalTemp (city = 'London') {
     $.getJSON(assemble_url(city, key), function(data) {
         $("#localTemp").html(data.main.temp);
         $("#cityName").html(data.name);
     });
   };
-
-  getLocalTemp();
 
   function update_ps_color() {
     var color = thermostat.isPowerSavingMode() ? "green" : "red";
@@ -30,6 +34,31 @@ $(document).ready(function() {
     var text = thermostat.isPowerSavingMode() ? "On" : "Off";
     $("#psm_state").html(text);
   };
+
+  function getTempFromApi() {
+    $.get('/temperature', function(data) {
+      if (data.temperature === null) {
+        $('#temp').html(thermostat.tempe);
+      } else {
+        $('#temp').html(data.temperature);
+      }
+    });
+  }
+
+  function getCityFromApi() {
+    $.get('/temperature', function(data) {
+      if (data.city === null) {
+        $('#cityName').html('London');
+        getLocalTemp();
+      } else {
+        $('#cityName').html(data.city);
+        getLocalTemp(data.city);
+      }
+    });
+  }
+
+  getTempFromApi();
+  getCityFromApi();
 
   function update_temperature() {
     $("#temp").html(thermostat.temp());
@@ -45,13 +74,20 @@ $(document).ready(function() {
     update_ps_text();
     update_ps_color();
     usageColor();
+    postingTemp(thermostat.temp());
   };
+
+  function postCity(city) {
+    $.post('/city', {city: city}, function(data) {
+    })
+  }
 
   update();
 
   $('#city_button').click(function() {
     var city = $('#city').val();
     getLocalTemp(city);
+    postCity(city);
   });
 
   $("#down").click(function() {
